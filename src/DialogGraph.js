69f69c781edafc8ce1bcd2c1f37df2d1e72908cc
Benjamin Degenhart <benjamin.degenhart@digitalservice.bund.de>
2022-05-19 23:58:26 +0200

    Work on the DialogGraph-structure

--- src/DialogGraph.js
+++ src/DialogGraph.js
@@ -4,19 +4,41 @@ class DialogGraph {
         this.nodes = {};
         this.edges = {};
         this.root = this.addNode("root");
-        let setupPhaseNode = this.addNode("SetupPhase");
-        this.addEdge("Setup phase required?", this.root, setupPhaseNode);
-        let reminderSetupNode = this.addNode("ReminderSetup");
-        this.addEdge("Setup reminder required?", setupPhaseNode, reminderSetupNode);
-        let troiSetupNode = this.addNode("TroiSetup");
-        this.addEdge("Setup troi required?", setupPhaseNode, troiSetupNode);
-        let determineCalcPosNode = this.addNode("DetermineCalcPos");
-        this.addEdge("Determining calculation position required?", troiSetupNode, determineCalcPosNode);
+
+        let setupPhase = this.addNode("SetupPhase",
+            "Let's setup some things, it won't take long.");
+        this.addEdge("auto", this.root, setupPhase);
+
+        let reminderSetup = this.addNode("ReminderSetup");
+        this.addEdge("2", setupPhase, reminderSetup);
+
+        let determinePositions = this.addNode("DeterminePositions",
+            "Alright, let's find out where your time bookings should go. " +
+            "I found these positions that you previously booked times on: xyz, is this still accurate or is something missing or meanwhile outdated? " +
+            "Ok, I would like to know which projects you are on and then please help me identify the so called \"positions\" within those projects " +
+            "that you are booking time on.");
+        this.addEdge("yes", determinePositions, reminderSetup);
+
+        let troiSetup = this.addNode("TroiSetup",
+            "Are you planning to use me (sometimes) for booking your time in Troi" +
+            " or do you only want me to remind you regularly to book your hours (in another tool)?");
+        this.addEdge("1", setupPhase, troiSetup);
+        this.addEdge("yes", troiSetup, determinePositions);
+        this.addEdge("no", troiSetup, reminderSetup);
+
+        let determineProjects = this.addNode("DetermineProjects");
+        this.addEdge("no", determinePositions, determineProjects);
+
+        let determineRole = this.addNode("DetermineRole");
+        this.addEdge("auto", determineProjects, determineRole);
+
+        let determinePositionsWithinProjects = this.addNode("DeterminePositionsWithinProjects");
+        this.addEdge("auto", determineRole, determinePositionsWithinProjects);
     }
 
-    addNode(name) {
+    addNode(name, prompt) {
         let id = Object.keys(this.nodes).length;
-        let node = new Node(id, name);
+        let node = new Node(id, name, prompt);
         this.nodes[id] = node;
         return node;
     }
@@ -41,9 +63,10 @@ class DialogGraph {
 }
 
 class Node {
-    constructor(id, name) {
+    constructor(id, name, prompt) {
         this.id = id;
         this.name = name;
+        this.prompt = prompt;
     }
 }
 
